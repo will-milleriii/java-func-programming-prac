e5584b6c5cd6e3b19929ce4cf5411507ee4c4cca
will <willmiller629@gmail.com>
2021-04-21 18:20:35 -0400

    chaining example

--- src/main/java/FunctionalInterface/_Function.java
+++ src/main/java/FunctionalInterface/_Function.java
@@ -9,11 +9,22 @@ public class _Function {
 
         int funcInc = incByOneFunc.apply(1);
         System.out.println(funcInc);
+
+        int multFunc = multByTen.apply(funcInc);
+        System.out.println(multFunc);
+
+        int addMult = addThenMultFunc.apply(2);
+        System.out.println(addMult);
     }
 
     //this function is the same as the increment method below
 
     static Function<Integer, Integer> incByOneFunc = num -> num + 1;
+
+    static Function<Integer, Integer> multByTen = num -> num * 10;
+
+    //use andThen function to execute multiple functions inline
+    static Function<Integer, Integer> addThenMultFunc = incByOneFunc.andThen(multByTen);
 
     static int increment(int num){
         return num + 1;
