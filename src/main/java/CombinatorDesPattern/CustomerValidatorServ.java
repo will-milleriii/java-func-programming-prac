45ef21a5f4b67b1699fb08b806eebcb8343b939d
will <willmiller629@gmail.com>
2021-04-22 10:05:45 -0400

    dummy validator

--- src/main/java/CombinatorDesPattern/CustomerValidatorServ.java
+++ src/main/java/CombinatorDesPattern/CustomerValidatorServ.java
@@ -1,4 +1,19 @@
 package CombinatorDesPattern;
 
+import java.time.LocalDate;
+import java.time.Period;
+
 public class CustomerValidatorServ {
+
+    public boolean isValidEmail(String email){
+        return email.contains("@");
+    }
+
+    public boolean isValidPhoneNum(String phoneNum){
+        return phoneNum.startsWith("+0");
+    }
+
+    public boolean isValidDob(LocalDate dob){
+        return Period.between(dob, LocalDate.now()).getYears() > 16;
+    }
 }
---
+++ src/main/java/CombinatorDesPattern/Main.java
@@ -0,0 +1,8 @@
+package CombinatorDesPattern;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+}
